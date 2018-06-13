f903dec7f132b114cacb669f82a15d8aeb30c516
Gabriel Valle <gvalle91@gmail.com>
2018-06-13 23:34:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.size = 0
+    puts "The line is currently empty."
+  else
+    output = "The line is curretnly: "
+    array.each do |name|
+      output = output + "#{array.indexOf(name)}. #{name} "
+    end
+  end
+end
