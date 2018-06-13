c7cccc53bf73a54a3430f115177e2241a0cfc7f6
Gabriel Valle <gvalle91@gmail.com>
2018-06-13 23:36:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else
-    output = "The line is curretnly: "
+    output = "The line is curretnly:"
     array.each do |name|
-      output = output + "#{array.index(name)}. #{name} "
+      output = output + " #{array.index(name)}. #{name}"
     end
     puts output
   end
