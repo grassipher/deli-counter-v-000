db7f8ca0ca2593853296af0ea956b2a582b16a91
Gabriel Valle <gvalle91@gmail.com>
2018-06-13 23:39:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,8 @@ def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.size} in line."
 end
+
+def now_serving(array)
+  puts "Currently serving #{array[0]}"
+  array.shift
+end
